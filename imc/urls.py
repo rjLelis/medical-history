13918d718b7f68bcc621aefd5cce25c339231a5e
rjLelis <renatojlelis@gmail.com>
2019-12-27 17:51:01 -0300

    Move existing api to v1

--- imc/urls.py
+++ imc/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('users/', views.ProfileWeightImcListCreateView.as_view(),
+    path('v1/users/', views.ProfileWeightImcListCreateView.as_view(),
         name='user-list-create'),
-    path('users/<str:username>/imc/', views.ProfileImcRetrieveView.as_view(),
+    path('v1/users/<str:username>/imc/', views.ProfileImcRetrieveView.as_view(),
         name='imc-list-create'),
-    path('users/<str:username>/weight',
+    path('v1/users/<str:username>/weight',
         views.ProfileWeightRetrieveView.as_view(),
         name='user-weight-history'),
+    path('v1/users/<str:username>/',
+        views.ProfileWeightImcRetrieveView.as_view(),
+        name='user-retrieve-update'),
 ]
