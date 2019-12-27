6c1d1cec1bbc4b2be0675c9ca2ab66c9c8fce4f9
rjLelis <renatojlelis@gmail.com>
2019-12-27 17:50:21 -0300

    Create a serializer util module

---
+++ imc/serializers_utils.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+
+class BaseProfile:
+    '''
+    Inherited whenever a Profile is the main model on the serializer
+    '''
+
+    def get_full_name(self, obj):
+        '''
+        Use serializers.SerializerMethodField(source='get_full_name')
+        '''
+        return f'{obj.first_name} {obj.last_name}'
+
+
+    def get_age(self, obj):
+        '''
+        Use serializers.SerializerMethodField(source='get_age')
+        '''
+        return date.today().year - obj.date_of_birth.year
+
