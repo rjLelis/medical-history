2be1ef41067b2a1379b2e71501845e71f483b3db
rjLelis <renatojlelis@gmail.com>
2020-03-20 18:04:38 -0300

    Improve documentation on the BaseProfile class

--- imc/serializers_utils.py
+++ imc/serializers_utils.py
@@ -8,13 +8,15 @@ class BaseProfile:
 
     def get_full_name(self, obj):
         """
-        Use serializers.SerializerMethodField(source='get_full_name')
+        Returns the full name of a Profile
+        use full_name = serializers.SerializerMethodField()
         """
         return f'{obj.first_name} {obj.last_name}'
 
     def get_age(self, obj):
         """
-        Use serializers.SerializerMethodField(source='get_age')
+        Returns the age of a Profile
+        use age = SerializerMethodField(source='get_age')
         """
         return date.today().year - obj.date_of_birth.year \
             if obj.date_of_birth is not None else ''
